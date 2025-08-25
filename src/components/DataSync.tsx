b08b0c2fdd3dae0283d9387622a4ecf9a48b42f8
Chenyiwei <chenyiwei10@hotmail.com>
2025-08-25 11:44:30 +0800

    fix: 移除未使用的SyncResponse类型导入

--- src/components/DataSync.tsx
+++ src/components/DataSync.tsx
@@ -4,8 +4,7 @@ import {
   downloadData, 
   generateBindCode, 
   getLocalBindCode,
-  clearLocalBindCode,
-  type SyncResponse 
+  clearLocalBindCode
 } from '../utils/syncUtils';
 
 interface DataSyncProps {
